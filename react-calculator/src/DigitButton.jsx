1bc8f93e5b24a6a246b323636a35990dd077125f
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-09-20 10:07:32 +1200

    Create functionality

---
+++ react-calculator/src/DigitButton.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { ACTIONS } from "./action";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
